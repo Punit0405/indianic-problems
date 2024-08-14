aea61db07966523b0e87702325ad584e9a2d7917
Punit Tewani <punit.k.tewani@unboundxinc.com>
2024-08-14 16:55:41 +0530

    feat: Approch and Unit Test cases describe and it descriptions changed.

--- src/problem.ts
+++ src/problem.ts
@@ -1,15 +1,27 @@
 const arr:number[] = [6, 13, 22, 18, 0, 3, 45, 57, 51, 5, 12, 55, 60];
 
-export const secondLargestElement1 = (arr: number[]): number | string => {
-    //first of all we are finding the maximum element from the array
-    const maxOfArray:number = Math.max(...arr);
+export const secondLargestElement1 = (arr: number[] | undefined): number | string => {
+    if(!arr){
+        return "Please pass an array";
+    }
+    if(arr.length < 1){
+        return "Please pass an array with at least 1 elements";
+    }
+    //Set the arrays first element as the maximum
+    let firstMax:number = arr[0];
 
     //declaring the minimum integer value;
-    let secondMax: number = (Number as any).MIN_SAFE_INTEGER
+    let secondMax: number = (Number as any).MIN_SAFE_INTEGER;
+
+    //Looping the array.
     for (const ele of arr) {
         // if the current element is greater than the maximum element
         // then we are assigning the current element as the maximum element
-        if (ele > secondMax && ele !== maxOfArray) {
+        //also we are checking the secondmax element.
+        if(ele > firstMax){
+            secondMax = firstMax;
+            firstMax = ele;
+        }else if (ele > secondMax && ele < firstMax) {
             secondMax = ele;
         }
     }
@@ -21,9 +33,13 @@ export const secondLargestElement1 = (arr: number[]): number | string => {
     return secondMax;
 }
 
-export const secondLargestElement2 = (arr: number[]): number | string => {
-    console.log("Running second approch");
-
+export const secondLargestElement2 = (arr: number[] | undefined): number | string => {
+    if(!arr){
+        return "Please pass an array";
+    }
+    if(arr.length < 1){
+        return "Please pass an array with at least 1 elements";
+    }
     // declaring the  first minimum integer value;
     let firstMax: number = (Number as any).MIN_SAFE_INTEGER;
     // declaring the second  minimum integer value;
--- test/problem.spec.ts
+++ test/problem.spec.ts
@@ -1,54 +1,91 @@
 import { expect } from 'chai';
 import { secondLargestElement1, secondLargestElement2 } from '../src/problem';
 
-describe('Example Test', () => {
-  it('Problem 1 Approch 1 Testing 1', () => {
+describe('Assigning First Element as Maximum', () => {
+  it('Passing the array and expecting the 57 as the second largest', () => {
     const arr:number[] = [6, 13, 22, 18, 0, 3, 45, 57, 51, 5, 12, 55, 60];
     const secondLargestElement = secondLargestElement1(arr);
     expect(secondLargestElement).to.equal(57);
   });
 
-  it('Problem 1 Approch 1 Testing 2', () => {
+  it('Passing the array and expecting the 60 as the second largest because of 100', () => {
     const arr:number[] = [6, 13, 22, 18, 0, 3, 45, 57, 51, 5, 12, 55, 60,100];
     const secondLargestElement = secondLargestElement1(arr);
     expect(secondLargestElement).to.equal(60);
   });
 
-  it('Problem 1 Approch 1 Testing 3', () => {
+  it('Passing the array and expecting 100 is not the second largest', () => {
     const arr:number[] = [6, 13, 22, 18, 0, 3, 45, 57, 51, 5, 12, 55, 60,100];
     const secondLargestElement = secondLargestElement1(arr);
     expect(secondLargestElement).not.eq(100);
   });
 
-  it('Problem 1 Approch 1 Testing 4', () => {
+  it('Passing the array with the same elements and expecting no second max element founded in return.', () => {
     const arr:number[] = [10,10,10];
     const secondLargestElement = secondLargestElement1(arr);
     expect(secondLargestElement).not.eq(100);
     expect(secondLargestElement).to.equal('no second max element founded');
   });
-
-  it('Problem 1 Approch 2 Testing 1', () => {
+  it('Passing two elements array with the same elements and expecting no second max element founded in return.', () => {
+    const arr:number[] = [9,10];
+    const secondLargestElement = secondLargestElement1(arr);
+    expect(secondLargestElement).not.eq(100);
+    expect(secondLargestElement).to.equal(9);
+  });
+  it('Passing the empty array and expecting Please pass an array with at least 1 elements in return ', () => {
+    const arr:number[] = [];
+    const secondLargestElement = secondLargestElement1(arr);
+    expect(secondLargestElement).not.eq(100);
+    expect(secondLargestElement).to.equal('Please pass an array with at least 1 elements');
+  });
+  it('Passing the undefined and expecting Please pass an array in return ', () => {
+    const arr:number[] | undefined = undefined;
+    const secondLargestElement = secondLargestElement1(arr);
+    expect(secondLargestElement).not.eq(100);
+    expect(secondLargestElement).to.equal('Please pass an array');
+  });
+});
+describe('Assigning First and Second highest element as -Infinity', () => {
+  it('Passing the array and expecting the 57 as the second largest using secondLargestElement2.', () => {
     const arr:number[] = [6, 13, 22, 18, 0, 3, 45, 57, 51, 5, 12, 55, 60];
     const secondLargestElement = secondLargestElement2(arr);
     expect(secondLargestElement).to.equal(57);
   });
 
-  it('Problem 1 Approch 2 Testing 2', () => {
+  it('Passing the array and expecting the 60 as the second largest because of 100 using secondLargestElement2.', () => {
     const arr:number[] = [6, 13, 22, 18, 0, 3, 45, 57, 51, 5, 12, 55, 60,100];
     const secondLargestElement = secondLargestElement2(arr);
     expect(secondLargestElement).to.equal(60);
   });
 
-  it('Problem 1 Approch 2 Testing 3', () => {
+  it('Passing the array and expecting 100 is not the second largest using secondLargestElement2.', () => {
     const arr:number[] = [6, 13, 22, 18, 0, 3, 45, 57, 51, 5, 12, 55, 60,100];
     const secondLargestElement = secondLargestElement2(arr);
     expect(secondLargestElement).not.eq(100);
   });
   
-  it('Problem 1 Approch 2 Testing 4', () => {
+  it('Passing the array with the same elements and expecting no second max element founded in return using secondLargestElement2.', () => {
     const arr:number[] = [10,10,10];
     const secondLargestElement = secondLargestElement2(arr);
     expect(secondLargestElement).not.eq(100);
     expect(secondLargestElement).to.equal('no second max element founded');
+  });
+  it('Passing two elements array with the same elements and expecting no second max element founded in return using secondLargestElement2.', () => {
+    const arr:number[] = [9,10];
+    const secondLargestElement = secondLargestElement2(arr);
+    expect(secondLargestElement).not.eq(100);
+    expect(secondLargestElement).to.equal(9);
+  });
+  it('Passing the empty array and expecting Please pass an array with at least 1 elements in return using secondLargestElement2. ', () => {
+    const arr:number[] = [];
+    const secondLargestElement = secondLargestElement2(arr);
+    expect(secondLargestElement).not.eq(100);
+    expect(secondLargestElement).to.equal('Please pass an array with at least 1 elements');
+  });
+  it('Passing the undefined and expecting Please pass an array in return using secondLargestElement2. ', () => {
+    const arr:number[] | undefined = undefined;
+    const secondLargestElement = secondLargestElement2(arr);
+    expect(secondLargestElement).not.eq(100);
+    expect(secondLargestElement).to.equal('Please pass an array');
   });
 });
